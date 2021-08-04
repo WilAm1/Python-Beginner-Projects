910920f1e29386503dbde13464545d855692de22
Wil <wilhelmjanmapogi@gmail.com>
2021-08-04 14:20:00 +0800

    "Add support for user interface in the terminal if run script"

--- FibonacciSequence/main.py
+++ FibonacciSequence/main.py
@@ -2,14 +2,21 @@
 
 
 def makeFibonacci(n):
+    """Returns integer of the sum of fibonacci sequence. Initial sides are at 0 and 1."""
     n1 = 0
     n2 = 1
     for _ in range(n):
-        n3 = n1+n2
+        n3 = n1 + n2
         n1 = n2
         n2=n3
     return n3
 
-result = makeFibonacci(6)
-print(result)
-
+if __name__ == '__main__':
+    """The program wil run if executed natively."""
+    while True:
+        number = int(input("Please input the number for iterations you want in integer only. : "))
+        print(makeFibonacci(number))
+        if input('Do you want to use the program again? Type "yes" to continue :  ').lower() == 'yes':
+            continue
+        else:
+            break
