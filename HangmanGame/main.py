62b642898f04d3b05f12ed268e620121a477135c
Wil <wilhelmjanmapogi@gmail.com>
2021-08-06 19:31:30 +0800

    Add basic algo for the game

--- HangmanGame/main.py
+++ HangmanGame/main.py
@@ -2,4 +2,17 @@
 a program that selects a random word 
 and then allows the user to guess it in a game of hangman."""
 
-
+word = 'hangman'
+blank_word = "_"*len(word)
+print('The word:  ',blank_word)
+guess_letter = input("Guess the letter: ").lower()
+blank_word_list = [i for i in blank_word]
+if guess_letter in word:
+    print("noice")
+    for index, letter in enumerate(blank_word_list):
+        if index == word.index(guess_letter):
+            blank_word_list[index] = guess_letter
+            blank_word = ''.join(blank_word_list)
+            continue
+            
+    print(blank_word)
