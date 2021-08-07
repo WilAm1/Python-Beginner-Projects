d1d1ec6cc142604fce5a55d53105fb5482250a3f
Wil <wilhelmjanmapogi@gmail.com>
2021-08-07 08:59:41 +0800

    complete basic structure

--- HangmanGame/main.py
+++ HangmanGame/main.py
@@ -4,30 +4,38 @@ and then allows the user to guess it in a game of hangman."""
 
 word = 'hangman'
 blank_word = "_"*len(word)
-print('The word:  ',blank_word)
+
 
 blank_word_list = [i for i in blank_word]
 lives = 5
 playing = True
 
 while playing:
-    guess_letter = input("Guess the letter: ").lower()
-    if lives > 0 and '_' in blank_word:
+    
+    print(f"Lives : {lives}")
+    print('The word:',  " ".join(blank_word_list))
+    if lives > 0 and '_' in blank_word_list:
+        guess_letter = input("Guess the letter: ").lower()
         if guess_letter in word:
+
             for index, letter in enumerate(word):
+
                 if letter == guess_letter:
                     blank_word_list[index] = letter
-                    print(blank_word_list)
+                    blank_word = ''.join(blank_word_list)
         else:
             lives -= 1
             print("you lose a life")
+            print(f"You only have {lives} lives")
     else:
+        if '_' in blank_word:
+            print("You lose!")
+        else:
+            print("You win!")
+
         print("Game Over")
         playing = False
     
-    blank_word = ''.join(blank_word_list)
+    
 
-            
-            
-    print(blank_word)
-    print(f"You only have {lives} lives")
+    
