c1f695c6d5e540338af1fc1ae0726dd6440de6e4
Wil <wilhelmjanmapogi@gmail.com>
2021-08-07 08:47:35 +0800

    Add while loop and basic structure

--- HangmanGame/main.py
+++ HangmanGame/main.py
@@ -5,17 +5,29 @@ and then allows the user to guess it in a game of hangman."""
 word = 'hangman'
 blank_word = "_"*len(word)
 print('The word:  ',blank_word)
-guess_letter = input("Guess the letter: ").lower()
-blank_word_list = [i for i in blank_word]
 
-if guess_letter in word:
-    print("noice")
-    for index, letter in enumerate(word):
-        if letter == guess_letter:
-            blank_word_list[index] = letter
-            print(blank_word_list)
+blank_word_list = [i for i in blank_word]
+lives = 5
+playing = True
+
+while playing:
+    guess_letter = input("Guess the letter: ").lower()
+    if lives > 0 and '_' in blank_word:
+        if guess_letter in word:
+            for index, letter in enumerate(word):
+                if letter == guess_letter:
+                    blank_word_list[index] = letter
+                    print(blank_word_list)
+        else:
+            lives -= 1
+            print("you lose a life")
+    else:
+        print("Game Over")
+        playing = False
+    
     blank_word = ''.join(blank_word_list)
 
             
             
     print(blank_word)
+    print(f"You only have {lives} lives")
