8db62d53045dfc43954818d08ec7f41c7b8a362c
Wil <wilhelmjanmapogi@gmail.com>
2021-08-08 12:27:22 +0800

    Add folder GuessTheNumber

---
+++ GuessTheNumber/main.py
@@ -0,0 +1,12 @@
+"""What’s My Number?
+
+Between 1 and 1000, there is only 1 number that meets the following criteria:
+
+    The number has two or more digits.
+    The number is prime.
+    The number does NOT contain a 1 or 7 in it.
+    The sum of all of the digits is less than or equal to 10.
+    The first two digits add up to be odd.
+    The second to last digit is even and greater than 1.
+    The last digit is equal to how many digits are in the number.
+    """
